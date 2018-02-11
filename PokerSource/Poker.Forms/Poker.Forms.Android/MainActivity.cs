4943901e8ddf1f51c52301d904c84f08ce8c9072
Steve Yeager <steve.yeager@littlebytegames.com>
2018-02-11 13:26:43 -0800

    Change build name to Randominder

--- PokerSource/Poker.Forms/Poker.Forms.Android/MainActivity.cs
+++ PokerSource/Poker.Forms/Poker.Forms.Android/MainActivity.cs
@@ -1,16 +1,11 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
 
 namespace Poker.Forms.Droid
 {
-    [Activity(Label = "Poker.Forms", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    [Activity(Label = "Randominder", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
         {
@@ -19,9 +14,8 @@ namespace Poker.Forms.Droid
 
             base.OnCreate(bundle);
 
-            global::Xamarin.Forms.Forms.Init(this, bundle);
+            Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
         }
     }
 }
-
