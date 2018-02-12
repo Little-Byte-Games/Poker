46116e20e602e1ca002e429ded013d4967a5daa6
Steve Yeager <steve.yeager@littlebytegames.com>
2018-02-11 21:53:15 -0800

    Add VS App Center analytics

--- PokerSource/Poker.Forms/Poker.Forms/App.xaml.cs
+++ PokerSource/Poker.Forms/Poker.Forms/App.xaml.cs
@@ -1,34 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Xamarin.Forms;
+﻿#if !DEBUG
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+#endif
 
 namespace Poker.Forms
 {
-	public partial class App : Application
-	{
-		public App ()
-		{
-			InitializeComponent();
+    public partial class App
+    {
+        public App()
+        {
+            InitializeComponent();
 
-			MainPage = new Poker.Forms.MainPage();
-		}
+            MainPage = new MainPage();
+        }
 
-		protected override void OnStart ()
-		{
-			// Handle when your app starts
-		}
+        protected override void OnStart()
+        {
+#if !DEBUG
+            AppCenter.Start("android=7dfc628b-a3ef-4e07-92c3-7abe7a646971;", typeof(Analytics), typeof(Crashes)); 
+#endif
+        }
 
-		protected override void OnSleep ()
-		{
-			// Handle when your app sleeps
-		}
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
 
-		protected override void OnResume ()
-		{
-			// Handle when your app resumes
-		}
-	}
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
 }
