9acb112287c942e14c4ccf843c5e0ab234ec8ea6
Unknown <sam.williams@littlebytegames.com>
2018-03-28 23:05:19 -0700

    Add 10 hex colors

--- PokerSource/Poker.Forms/Poker.Forms/Global/Colors.cs
+++ PokerSource/Poker.Forms/Poker.Forms/Global/Colors.cs
@@ -6,10 +6,16 @@ namespace Poker.Forms.Global
     {
         public static readonly Color[] ThemeColors =
         {
-            Color.Aquamarine,
-            Color.Crimson,
-            Color.DarkSeaGreen,
-            Color.IndianRed,
+            Color.FromHex("#bc2c00"),
+            Color.FromHex("#db6900"),
+            Color.FromHex("#efb713"),
+            Color.FromHex("#52b72d"),
+            Color.FromHex("#30c1b5"),
+            Color.FromHex("#3060c1"),
+            Color.FromHex("#6030c1"),
+            Color.FromHex("#d86ed1"),
+            Color.FromHex("#ddd4dd"),
+            Color.FromHex("#49494f"),
         };
     }
 }
